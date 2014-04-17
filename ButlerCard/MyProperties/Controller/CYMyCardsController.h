309bb0017a4cd5af2edbab08a343ca50b70436af
CaiYang <774164@qq.com>
2014-04-17 11:31:32 +0800

    我的业主卡UI基本搭建完毕

---
+++ ButlerCard/MyProperties/Controller/CYMyCardsController.h
@@ -0,0 +1,13 @@
+//
+//  CYMyCardsController.h
+//  ButlerCard
+//
+//  Created by niko on 14-4-17.
+//  Copyright (c) 2014年 johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYMyCardsController : UITableViewController
+
+@end
