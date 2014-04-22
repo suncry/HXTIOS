5a57e1558b919beafd43bdaec47b98ab83443097
CaiYang <774164@qq.com>
2014-04-22 17:24:01 +0800

    整理了文件的位置。

---
+++ ButlerCard/SurroundingLife/Controller/CYSurroundingLifeController.h
@@ -0,0 +1,15 @@
+//
+//  CYSurroundingLifeController.h
+//  ButlerCard
+//
+//  Created by niko on 14-4-22.
+//  Copyright (c) 2014年 johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYSurroundingLifeController : UITableViewController
+@property (weak, nonatomic) IBOutlet UIButton *styleBtn;
+- (IBAction)styleBtnClick:(id)sender;
+
+@end
