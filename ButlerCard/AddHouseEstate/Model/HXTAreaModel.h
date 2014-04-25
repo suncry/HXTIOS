3122e588782c7691b9b244ce3a88dc05605492a4
唐友亮 <tangyl@enveesoft.com>
2014-04-25 16:08:35 +0800

    添加HXTAreaModel对象

---
+++ ButlerCard/AddHouseEstate/Model/HXTAreaModel.h
@@ -0,0 +1,13 @@
+//
+//  HXTAreaModel.h
+//  ButlerCard
+//
+//  Created by johnny tang on 4/25/14.
+//  Copyright (c) 2014 johnny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HXTAreaModel : NSObject
+
+@end
---
+++ ButlerCard/AddHouseEstate/Model/HXTAreaModel.m
@@ -0,0 +1,14 @@
+//
+//  HXTAreaModel.m
+//  ButlerCard
+//
+//  Created by johnny tang on 4/25/14.
+//  Copyright (c) 2014 johnny. All rights reserved.
+//
+
+#import "HXTAreaModel.h"
+#import "AFNetworking.h"
+
+@implementation HXTAreaModel
+
+@end
