a1f540712c638bf09334e7cdfc64df74276f5c3f
CaiYang <774164@qq.com>
2014-04-28 15:47:58 +0800

    地图上加入大头针。

--- ButlerCard/SurroundingLife/Controller/CYShopMapController.h
+++ ButlerCard/SurroundingLife/Controller/CYShopMapController.h
@@ -9,10 +9,19 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import "HXTAppDelegate.h"
 
 @interface CYShopMapController : UIViewController<MKMapViewDelegate, CLLocationManagerDelegate>
  {
     CLLocationManager *locationManager;
  }
+//用于CoreData
+@property (strong,nonatomic) HXTAppDelegate *myDelegate;
 @property (weak, nonatomic) IBOutlet MKMapView *shopMap;
+
+@property (retain,nonatomic) NSString *shopName;
+@property (retain,nonatomic) NSString *shopAddress;
+@property (retain,nonatomic) NSNumber *shopLatitude;
+@property (retain,nonatomic) NSNumber *shopLongitude;
+
 @end
--- ButlerCard/SurroundingLife/Controller/CYShopMapController.m
+++ ButlerCard/SurroundingLife/Controller/CYShopMapController.m
@@ -8,6 +8,8 @@
 
 #import "CYShopMapController.h"
 #import "CYCustomAnnotation.h"
+#import "Shop.h"
+
 @interface CYShopMapController ()
 
 @end
@@ -22,10 +24,16 @@
     }
     return self;
 }
-
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self queryFromDB];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //获取当前应用程序的委托（UIApplication sharedApplication为整个应用程序上下文）
+    self.myDelegate = (HXTAppDelegate *)[[UIApplication sharedApplication] delegate];
+
     
 //    _shopMap = [[MKMapView alloc] initWithFrame:[self.view bounds]];
     //显示地图
@@ -33,7 +41,7 @@
     _shopMap.mapType = MKMapTypeStandard;
     [self.view addSubview:_shopMap];
     //定位
-    CLLocationCoordinate2D coords = CLLocationCoordinate2DMake(39.915352,116.397105);
+    CLLocationCoordinate2D coords = CLLocationCoordinate2DMake([_shopLatitude doubleValue],[_shopLongitude doubleValue]);
     float zoomLevel = 0.02;
     MKCoordinateRegion region = MKCoordinateRegionMake(coords, MKCoordinateSpanMake(zoomLevel, zoomLevel));
     [_shopMap setRegion:[_shopMap regionThatFits:region] animated:YES];
@@ -50,9 +58,37 @@
 {
     CYCustomAnnotation *annotation = [[CYCustomAnnotation alloc] initWithCoordinate:
                                     coords];
-    annotation.title = @"标题";
-    annotation.subtitle = @"子标题";
+    annotation.title = _shopName;
+    annotation.subtitle = _shopAddress;
     [_shopMap addAnnotation:annotation];
+}
+- (void)queryFromDB
+{
+    //创建取回数据请求
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    //设置要检索哪种类型的实体对象
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Shop"inManagedObjectContext:self.myDelegate.managedObjectContext];
+    //设置请求实体
+    [request setEntity:entity];
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"(shopID = %@)",[[NSUserDefaults standardUserDefaults]valueForKey:kShopID]];
+    [request setPredicate:predicate];
+    NSError *error = nil;
+    //执行获取数据请求，返回数组
+    NSMutableArray *mutableFetchResult = [[self.myDelegate.managedObjectContext executeFetchRequest:request error:&error] mutableCopy];
+    if (mutableFetchResult == nil)
+    {
+        NSLog(@"Error: %@,%@",error,[error userInfo]);
+    }
+    NSLog(@"The count of FetchResult:%lu",(unsigned long)[mutableFetchResult count]);
+    for(Shop *shop in mutableFetchResult)
+    {
+        //        NSLog(@"Shop ------>  grade:%@",entry.grade);
+        _shopName = shop.name;
+        _shopAddress = shop.address;
+        _shopLatitude = shop.positionY;
+        _shopLongitude = shop.positionX;
+    }
 }
 
 @end
