08ea7a31df2ad8b17a83e5da67001493f0a4ee98
CaiYang <774164@qq.com>
2014-05-06 14:42:19 +0800

    将设置手势密码加入到系统设置重。

---
+++ ButlerCard/MyButler/CYSetGestureCodeController.h
@@ -0,0 +1,14 @@
+//
+//  CYSetGestureCodeController.h
+//  ButlerCard
+//
+//  Created by niko on 14-5-6.
+//  Copyright (c) 2014年 johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MJPasswordView.h"
+
+@interface CYSetGestureCodeController : UIViewController<MJPasswordDelegate>
+
+@end
