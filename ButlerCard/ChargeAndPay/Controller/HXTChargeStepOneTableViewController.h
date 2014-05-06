ee34335e2a38305d37f2c13ee4f7109d18a7111f
唐友亮 <tangyl@enveesoft.com>
2014-05-06 14:56:18 +0800

    完成充值UI布局

---
+++ ButlerCard/ChargeAndPay/Controller/HXTChargeStepOneTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXTChargeStepOneTableViewController.h
+//  ButlerCard
+//
+//  Created by johnny tang on 5/6/14.
+//  Copyright (c) 2014 johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXTChargeStepOneTableViewController : UITableViewController
+
+@end
---
+++ ButlerCard/ChargeAndPay/Controller/HXTChargeStepOneTableViewController.m
@@ -0,0 +1,150 @@
+//
+//  HXTChargeStepOneTableViewController.m
+//  ButlerCard
+//
+//  Created by johnny tang on 5/6/14.
+//  Copyright (c) 2014 johnny. All rights reserved.
+//
+
+#import "HXTChargeStepOneTableViewController.h"
+
+@interface HXTChargeStepOneTableViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *inputMoneyTestField;
+
+@end
+
+@implementation HXTChargeStepOneTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+//{
+//#warning Potentially incomplete method implementation.
+//    // Return the number of sections.
+//    return 0;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+//#warning Incomplete method implementation.
+//    // Return the number of rows in the section.
+//    return 0;
+//}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Local functions
+
+- (void)backgroundTouchUpInside {
+
+}
+
+#pragma mark - IB Actions
+
+- (IBAction)moneyButtonPressed:(UIButton *)sender {
+    switch (sender.tag) {
+        case 101:
+            NSLog(@"20元");
+            break;
+        case 102:
+            NSLog(@"50元");
+            break;
+        case 103:
+            NSLog(@"100元");
+            break;
+        default:
+            break;
+    }
+    
+}
+
+#pragma mark - Navigation
+/*
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)cancelBarButtonItemPressed:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:^{}];
+}
+
+@end
