c93e265f0d85eb05a69bb91ab2230b7902c169e3
CaiYang <774164@qq.com>
2014-05-09 11:34:22 +0800

    加入了二维码扫描界面。

---
+++ ButlerCard/MyButler/CYPayViewController.h
@@ -0,0 +1,14 @@
+//
+//  CYPayViewController.h
+//  ButlerCard
+//
+//  Created by niko on 14-5-9.
+//  Copyright (c) 2014年 johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYPayViewController : UIViewController
+- (IBAction)scanBtnClick:(id)sender;
+
+@end
